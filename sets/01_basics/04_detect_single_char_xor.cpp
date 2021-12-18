afb553df0f7d780ffa6e7050d05fe4e978a8e8b0
GuySuday <guys4000@gmail.com>
2021-12-18 18:45:49 +0200

    solved detect_single_char_xor

---
+++ sets/01_basics/04_detect_single_char_xor.cpp
@@ -0,0 +1,31 @@
+#include <fstream> // std::ifstream
+
+#include "03_single_byte_xor_cipher.hpp"
+#include "04_detect_single_char_xor.hpp"
+#include "../../utils/plaintext_score_utils.hpp"
+
+s01::c04::Result s01::c04::detect_single_char_xor(std::string& file_path) 
+{
+    std::ifstream file(file_path);
+    std::string hex_str, best_hex_str;
+    std::vector<BYTE> best_plaintext;
+    double max_score = 0;
+
+    while (std::getline(file, hex_str))
+    {
+        std::string plaintext_str = s01::c03::single_byte_xor_cipher(hex_str);
+        std::vector<BYTE> plaintext(plaintext_str.begin(), plaintext_str.end()); // TODO: shouldn't we just change single_byte_xor_cipher to return a vector?
+        double current_score = plaintext_score_utils::score_text(plaintext);
+        if (current_score > max_score)
+        {
+            max_score = current_score;
+            best_plaintext = plaintext;
+            best_hex_str = hex_str;
+        }
+    }
+    Result result = {
+        std::string(best_plaintext.begin(), best_plaintext.end()), // TODO: maybe just return a vector?
+        best_hex_str
+    };
+    return result;
+}
---
+++ sets/01_basics/04_detect_single_char_xor.hpp
@@ -0,0 +1,19 @@
+#ifndef SETS_01_BASICS_04_DETECT_SINGLE_CHAR_XOR_HPP_INCLUDED
+#define SETS_01_BASICS_04_DETECT_SINGLE_CHAR_XOR_HPP_INCLUDED
+
+#include <string> // std::string
+
+namespace s01
+{
+    namespace c04
+    {
+        struct Result
+        {
+            std::string plaintext;
+            std::string plaintext_hex;
+        };
+        Result detect_single_char_xor(std::string file_path);
+    };
+};
+
+#endif /* SETS_01_BASICS_04_DETECT_SINGLE_CHAR_XOR_HPP_INCLUDED */
